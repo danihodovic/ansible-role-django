d0257c9e9e9ef816641dc4b72c605e5a3b8840bc
Dani Hodovic <dani.hodovic@gmail.com>
2021-02-19 20:37:56 +0200

    Add celery deployment & migrate to AWS

--- tests/test_all.py
+++ tests/test_all.py
@@ -1,7 +1,44 @@
-def test_containers_running(host):
-    assert host.docker("django")
+# pylint: disable=redefined-outer-name,invalid-name
+import time
+
+import pytest
+from testinfra.host import Host
 
 
-def test_pings_django(host):
+@pytest.fixture(scope="session")
+def target_host(request):
+    def fn(host, sudo=True):
+        return Host.get_host(
+            f"ansible://{host}?ansible_inventory={request.config.option.ansible_inventory}",
+            sudo=sudo,
+        )
+
+    return fn
+
+
+def test_django_running(target_host):
+    host = target_host("web")
+    assert host.docker("django").is_running
+
+
+def test_pings_django(target_host):
+    host = target_host("web")
     result = host.run("curl http://localhost")
-    assert "Get started with Django" in result.stdout
+    assert "404" in result.stdout
+
+
+def test_celery_worker_running(target_host):
+    host = target_host("worker")
+    assert host.docker("celery-worker").is_running
+
+
+def test_celery_completes_task(target_host):
+    web_facts = target_host("web").ansible("setup")["ansible_facts"]
+    web_ip = web_facts["ansible_default_ipv4"]["address"]
+    worker = target_host("worker")
+    worker.ansible("uri", f"url=http://{web_ip}/task", check=False)
+    time.sleep(5)
+    output = worker.check_output(
+        f"echo 'keys *' | docker run -i redis redis-cli -h {web_ip}"
+    )
+    assert "celery-task-meta-" in output
